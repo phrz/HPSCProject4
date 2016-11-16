29cd95509bc7e2c8cfb33c0d6baaa2245c630176
Paul Herz <phrz@me.com>
2016-11-16 15:29:54 -0600

    Adaptive integral approximation, first draft

--- src/adaptive_int.cpp
+++ src/adaptive_int.cpp
@@ -6,4 +6,132 @@
 //  Copyright © 2016 Paul Herz. All rights reserved.
 //
 
-#include <stdio.h>
+#include <cmath>
+#include "println.cpp"
+#include "composite_int.cpp"
+
+using Fcn = std::function<double(double)>;
+
+// Call `composite_int` adaptively, such that
+//
+//     abs( I(f) - Rn(f) ) < rtol * abs( I(f) ) + atol
+//
+// Where I(f) is the integral, and Rn(f) is the `composite_int` function,
+// and computation is minimized to achieve the above condition.
+//
+// Because the true integral is not known, we can use the rapid convergence
+// of `composite_int` to approximate I(f) in a way that helps find `n`.
+// Beginning with k at an arbitrary value, update `n` until this
+// approximation is satisfied:
+//
+//     abs( Rn+k(f) - Rn(f) ) < rtol * abs( Rn+k(f) ) + atol
+
+template<typename T>
+int adaptive_int(T& f, const double a, const double b, const double rtol,
+				 const double atol, double& R, int& n, int& Ntot)
+{
+	// Compute the integral of f(x) over [a,b] relative to x adaptively,
+	// meeting an error bound condition while minimizing computation.
+	
+	// GIVEN:
+	// - [a,b], the integral bounds
+	// - rtol and atol, parameters of the error condition,
+	// - other parameters solely for output (R, n, Ntot)
+	
+	// EXPECTED BEHAVIOR:
+	// - `R` should be assigned an approximation.
+	// - `n` should be assigned the subinterval count that first satisfied
+	//   the error threshold in the iterative ('adaptive') process.
+	// - `Ntot` should be assigned the total number of subintervals evaluated
+	//   before and including a calculation satisfying the error condition.
+	// - The return value should be '0' if successful in satisfying the error
+	//   condition, and '1' otherwise.
+	
+	// SIDE EFFECTS:
+	// - `R`, `n`, and `Ntot` are guaranteed to be modified under successful
+	//   conditions.
+	// - Under failing conditions, `Ntot` will either be zero or an undefined
+	//   value.
+	// - Under failing conditions, `n` will be an undefined value.
+	// - Under failing conditions, `Ntot` will be zero or an undefined value.
+	
+	// STRATEGY: with an step counter i=0, n begins at 6, k = floor((i+1)^2)
+	// (i) Initialize return parameters
+	
+	// current integral approximation
+	R = 0;
+	
+	// total subintervals evaluated
+	Ntot = 0;
+	
+	// subintervals to evaluate on this step
+	n = 6;
+	
+	// (ii) Initialize other variables for calculation
+	
+	// step counter for iterative adaptation
+	int i = 0;
+	int iMax = 1e6;
+	
+	// value to increment `n` by at the end of each step.
+	// Defined as a function of `i` at each step.
+	int k = 0;
+	
+	// The next integral approximation after `R`, where `R` has `n` subintervals
+	// and `Rnext` has `n+k` subintervals - used to approximate I(f).
+	double Rnext = 0.0;
+	
+	// The flag used to continue iteration
+	bool underThreshold = false;
+	
+	// Absolute error function
+	auto error = [&](double a1, double a2) -> double {
+		return std::abs(a1-a2);
+	};
+	
+	// Upper bound condition for success given an integral I(f) or
+	// in this case an integral approximation.
+	auto bound = [&](double I) -> double {
+		return rtol * std::abs(I) + atol;
+	};
+	
+	while(!underThreshold && i <= iMax) {
+		// (a) Evaluative step: determine if error condition is satisfied.
+		
+		k = int(std::pow(i+1,5)); // heuristic incrementor
+		
+		// At i=0, calculate Rn and Rn+k. After that, shift Rn := Rn+k,
+		// then calculate just Rn+k, because n := n+k at the iterative step.
+		//
+		// i.e.:
+		//     i=0 -> 2n+k subinterval calculations,
+		//     i≠0 -> n+k subinterval calculations.
+		//
+		
+		if(i != 0) {
+			R = Rnext;
+		} else {
+			// Calculate Rn once at i=0, and record nº of subintervals to Ntot.
+			R = composite_int(f, a, b, n);
+			Ntot += n;
+		}
+		
+		// Calculate Rn+k for all i, and record nº of subintervals to Ntot.
+		Rnext = composite_int(f, a, b, n+k);
+		Ntot += n+k;
+		
+		underThreshold = (error(Rnext, R) < bound(Rnext));
+		
+		if(underThreshold) {
+			// All returning variables, `Ntot`, `n`, and `R` are set.
+			// Just exit before iterative step.
+			return 0;
+		}
+		
+		// (b) Iterative step: increment `i` and `n := n + k`.
+		++i;
+		n += k;
+	}
+	
+	return 1;
+}
---
+++ src/test_adapt.cpp
@@ -0,0 +1,76 @@
+//
+//  test_adapt.cpp
+//  HPSCProject4
+//
+//  Created by Paul Herz on 11/16/16.
+//  Copyright © 2016 Paul Herz. All rights reserved.
+//
+
+#include <vector>
+#include <cmath>
+#include "adaptive_int.cpp"
+#include "println.cpp"
+
+using namespace std;
+
+int main() {
+	
+	// Evaluate the same problem as `test_int`:
+	
+	// limits of integration
+	double a = -3.0;
+	double b = 5.0;
+	
+	double c = 0.5;
+	double d = 25.0;
+	
+	// function
+	Fcn f = [&](double x) {
+		return (exp(c*x) + sin(d*x));
+	};
+	
+	// antiderivative
+	Fcn F = [&](double x) {
+		return (exp(c*x)/c - cos(d*x)/d);
+	};
+	
+	// Let:
+	//
+	//     rtol = {10^-2, 10^-4, 10^-6 ..., 10^-12}
+	//     atol_i = rtol_i/1000
+	//
+	// For each set of tolerances, print |I(f)-R(f)| and rtol*|I(f)|+atol,
+	// as well as `n` and `Ntot`.
+	
+	size_t t_s = 6;
+	vector<double> rtol(t_s);
+	vector<double> atol(t_s);
+	
+	// Iterate once to build rtol and atol.
+	for(int i = 0; i < t_s; ++i) {
+		rtol[i] = std::pow(10.0, -2*(i+1));
+		atol[i] = rtol[i]/1000;
+	}
+	
+	size_t computationalCost = 0;
+
+	// Iterate again to test `adaptive_int`.
+	for(int i = 0; i < 6; ++i) {
+		println("rtol: ",rtol[i],"\tatol:",atol[i]);
+		
+		int Ntot, n;
+		double R;
+		int status = adaptive_int(f, a, b, rtol[i], atol[i], R, n, Ntot);
+		
+		if(status != 0) {
+			println("FAILURE!");
+		} else {
+			println("n:",n,"\tNtot:",Ntot,"\tR:",to_string(R),"\n\n");
+			computationalCost += Ntot;
+		}
+	}
+	
+	println("Total computational cost of this test:", computationalCost);
+	
+	return 0;
+}
