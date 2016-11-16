f62e9e7af7d23de2acef7634111759bf2a951338
Paul Herz <phrz@me.com>
2016-11-15 21:58:49 -0600

    C++14 standard, table display for data in STDOUT

--- src/test_int.cpp
+++ src/test_int.cpp
@@ -8,10 +8,46 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <experimental/optional>
+#include <string>
+#include <sstream>
+
 #include "println.cpp"
 #include "composite_int.cpp"
 
+template<class T>
+using optional = std::experimental::optional<T>;
+
 using namespace std;
+
+string rightPad(string s, size_t size, char pad = ' ') {
+	size_t originalSize = s.size();
+	if(originalSize == size) {
+		return s;
+	}
+	
+	// either truncate or pad the string depending
+	// on requested size vs actual size.
+	s.resize(size, pad);
+	
+	// if shrunken, replace last character with
+	// ellipsis character.
+	if(originalSize > size) {
+		s.pop_back();
+		s.append("…");
+	}
+		
+	return s;
+}
+
+template<typename T>
+string rightPad(T t, size_t size, char pad = ' ') {
+	stringstream stream;
+	stream << t;
+	auto s = stream.str();
+	return rightPad(s, size, pad);
+}
 
 int main(int argc, char* argv[]) {
 	
@@ -19,9 +55,53 @@ int main(int argc, char* argv[]) {
 	// to demonstrate the convergence rate of the method with n >= 6.
 	// Include this output in the report.
 	
-	Fcn f = [](double x){ return x*x; };
+	// limits of integration
+	double a = -3.0;
+	double b = 5.0;
 	
-	println(composite_int(f, 0, 1, 6),"\n");
+	double c = 0.5;
+	double d = 25.0;
+	
+	// function
+	Fcn f = [&](double x) {
+		return (exp(c*x) + sin(d*x));
+	};
+	
+	// antiderivative
+	Fcn F = [&](double x) {
+		return (exp(c*x)/c - cos(d*x)/d);
+	};
+	
+	double trueIntegral = F(b) - F(a);
+	println("True integral:", trueIntegral);
+	
+	vector<int> n = {20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240};
+	vector<double> errors(n.size());
+	vector<double> hvals(n.size());
+	optional<double> convergence;
+	
+	println("┌────────┬────────────┬───────────────┬───────────────┐");
+	println("| n      |   approx   |     error     |  convergence  |");
+	println("├────────┼────────────┼───────────────┼───────────────┤");
+	
+	for(size_t i = 0; i < n.size(); ++i) {
+		double approximateIntegral = composite_int(f, a, b, n[i]);
+		errors[i] = abs(trueIntegral-approximateIntegral)/abs(trueIntegral);
+		hvals[i] = (b-a)/n[i];
+		
+		string conv = "n/a";
+		if(convergence) {
+			conv = (*convergence);
+		}
+		
+		println("│ " + rightPad(n[i],7)
+		  +"│ " + rightPad(approximateIntegral,11)
+		  +"│ " + rightPad(errors[i],14)
+		  +"│ " + rightPad(conv, 14)
+		  +"│");
+	}
+	
+	println("└────────┴────────────┴───────────────┴───────────────┘");
 	
 	return 0;
 }
